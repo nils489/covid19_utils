b1a8fcf904a1868883987a02db0e758cbfcbb168
LNelson89 <LNelson@gmx.de>
2020-04-03 18:07:24 +0200

    added option to plot only new infections

--- plot_country_time_series.py
+++ plot_country_time_series.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -16,6 +17,11 @@ parser.add_argument('--log',
                     default=False,
                     help='Flag to determine whether or not to '
                     'plot logarithmic.')
+parser.add_argument('--new',
+                    type=bool,
+                    default=False,
+                    help='If True, plot only new cases. '
+                    'default: False')
 args = parser.parse_args()
 cr_key = args.country
 
@@ -30,13 +36,25 @@ line_country = ts[(ts['Country/Region'] == cr_key)]
 ts_country = line_country.drop(['Province/State',
                                 'Country/Region',
                                 'Lat', 'Long'], axis=1)
-np_ts_country = ts_country.values
+
 try:
-    # plot dataframe
-    ts_df = pd.DataFrame({'confirmed cases': ts_country.values[0],
-                          'dates': ts_country.columns})
-    lineplot = ts_df.plot.line(x='dates', y='confirmed cases', logy=args.log)
-    plt.show()
-    pass
+    np_line = ts_country.values[0]
 except IndexError:
     print("The specified country '" + cr_key + "' was not found!")
+    quit()
+
+# build dataframes for plotting
+ts_df = pd.DataFrame({'confirmed cases': np_line,
+                      'dates': ts_country.columns})
+# dataframe with new cases
+ts_new_list = [0]
+for ind in range(1, np_line.shape[0]):
+    ts_new_list.append(np_line[ind] - np_line[ind-1])
+ts_new_array = np.array(ts_new_list)
+ts_new_df = pd.DataFrame({'new cases': ts_new_array,
+                          'dates': ts_country.columns})
+if args.new:
+    lineplot = ts_new_df.plot.line(x='dates', y='new cases', logy=args.log)
+else:
+    lineplot = ts_df.plot.line(x='dates', y='confirmed cases', logy=args.log)
+plt.show()
