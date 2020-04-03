50f688cb786d7d0a3b721ed354fd4dfaee56c186
LNelson89 <LNelson@gmx.de>
2020-04-03 21:05:43 +0200

    outsourced dataframe preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,3 +8,11 @@ def load_time_series_data():
     ts_filename = 'time_series_covid19_confirmed_global.csv'
     ts_filepath = os.path.join(ts_path, ts_filename)
     return pd.read_csv(ts_filepath)
+
+
+def get_country_time_series_line(country, time_series_data_frame):
+    ts = time_series_data_frame
+    country_line = ts[(ts['Country/Region'] == country)]
+    return country_line.drop(['Province/State',
+                              'Country/Region',
+                              'Lat', 'Long'], axis=1)
--- plot_country_time_series.py
+++ plot_country_time_series.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from data_preprocessing import load_time_series_data
+from data_preprocessing import load_time_series_data, \
+                               get_country_time_series_line
 
 # parse arguments
 parser = argparse.ArgumentParser(
@@ -24,27 +25,24 @@ args = parser.parse_args()
 
 # build dataframe
 ts = load_time_series_data()
-line_country = ts[(ts['Country/Region'] == args.country)]
-ts_country = line_country.drop(['Province/State',
-                                'Country/Region',
-                                'Lat', 'Long'], axis=1)
+ts_line = get_country_time_series_line(args.country, ts)
 
 try:
-    np_line = ts_country.values[0]
+    np_line = ts_line.values[0]
 except IndexError:
     print("The specified country '" + args.country + "' was not found!")
     quit()
 
 # build dataframes for plotting
 ts_df = pd.DataFrame({'confirmed cases': np_line,
-                      'dates': ts_country.columns})
+                      'dates': ts_line.columns})
 # dataframe with new cases
 ts_new_list = [0]
 for ind in range(1, np_line.shape[0]):
     ts_new_list.append(np_line[ind] - np_line[ind-1])
 ts_new_array = np.array(ts_new_list)
 ts_new_df = pd.DataFrame({'new cases': ts_new_array,
-                          'dates': ts_country.columns})
+                          'dates': ts_line.columns})
 if args.new:
     lineplot = ts_new_df.plot.line(x='dates', y='new cases', logy=args.log)
 else:
